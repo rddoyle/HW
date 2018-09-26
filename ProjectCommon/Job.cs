ecf472d6ecce51803f57ed152e1deb3b3b2c2d17
Robert Doyle <rddoyle@roadrunner.com>
2018-09-26 13:08:05 -0400

    Added Job class

---
+++ ProjectCommon/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCommon
+{
+    public class Job
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+    }
+}
