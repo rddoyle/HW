478c4279de9352fe763d07ce623e0fd2a18696ab
Robert Doyle <rddoyle@roadrunner.com>
2018-09-26 13:11:31 -0400

    Added Feature1

---
+++ ProjectCommon/Feature1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCommon
+{
+    public class Feature1
+    {
+    }
+}
