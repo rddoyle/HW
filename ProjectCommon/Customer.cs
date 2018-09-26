885df9ea80665b57d74685283258c13cf6839681
Robert Doyle <rddoyle@roadrunner.com>
2018-09-26 13:03:29 -0400

    Added initial supoport for common class library.

---
+++ ProjectCommon/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCommon
+{
+    public class Customer
+    {
+        public Customer(string name, int id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        private string  _name;
+        private int     _id;
+    }
+}
